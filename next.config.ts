9b7c34a8c00a6ac15b01307eecb5a4be48d11bfe
donSebamarquez <sebamarquezblanco@gmail.com>
2025-02-03 22:49:35 -0300

    final2

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true, // Esto desactiva el linting durante la construcción
+  },
 };
 
 export default nextConfig;
